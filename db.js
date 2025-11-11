5b1b7a9606f64de762e0d11bd9f612a421b15570
finanovita <finanovita479@gmail.com>
2025-11-11 16:40:22 +0700

    menambahkan database

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
+const mysql = require('mysql2');
 
+// Konfigurasi koneksi ke MySQL
+const db = mysql.createConnection({
+  host: 'localhost',   // server MySQL
+  user: 'root',        // ganti sesuai user MySQL-mu
+  password: 'Balikpapan30',        // isi password MySQL jika ada
+  database: 'apikey_db' // nama database yang sudah dibuat
+});
+
+// Cek koneksi
+db.connect(err => {
+  if (err) {
+    console.error('Gagal terhubung ke MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database.');
+});
+
+// Export db agar bisa digunakan di index.js
+module.exports = db;
